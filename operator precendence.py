f629ada5d9bd50e2b4cdb9566d3afe150da1a35e
krish06m <reachkrishmathur@gmail.com>
2024-10-15 20:45:42 +0530

    oct15p2

---
+++ operator precendence.py
@@ -0,0 +1,27 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 4/(3*(2-1))
+1.3333333333333333
+>>> (2**3)**2
+64
+>>> 2**3**2
+512
+>>> float(22//3+3/3)
+8.0
+>>> 8/4/2
+1.0
+>>> (2&3)|(~5)
+-6
+>>> 2&3
+2
+>>> True and False or True
+True
+>>> True and False and True
+False
+>>> (4<5)and(5>=5)
+True
+>>> # * polymorphism
+>>> "hello" * 2
+'hellohello'
+>>> 4+2**5//10
+7
